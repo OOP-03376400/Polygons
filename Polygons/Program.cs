7555cb06f76ae9da4d9479f7fd56cd3c726058ae
harshvardhan1996 <harshamazon1996@gmail.com>
2018-06-07 01:28:26 +0530

    Concrete Class Example

--- Polygons/Program.cs
+++ Polygons/Program.cs
@@ -4,12 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Polygons
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var square = new Square(5);
+            DisplayPolygon("Square", square);
+            Console.ReadKey();
+        }
+        public static void DisplayPolygon(string polygonType, dynamic polygon)
+        {
+            try
+            {
+                Console.WriteLine("{0} Number of Slides: {1}", polygonType, polygon.NumberOfSides);
+                Console.WriteLine("{0} Side Length: {1}", polygonType, polygon.SideLength);
+                Console.WriteLine("{0} Perimeter: {1}", polygonType, polygon.GetPerimeter());
+                Console.WriteLine("{0} Area: {1}", polygonType, Math.Round(polygon.GetArea(), 2));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Exception thrown for {0} is {1}", polygonType, ex.GetType().Name);
+                
+            }
         }
     }
 }
